90ac42152361a8ac8e05c3b7940dc9e88137da7d
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-12-01 18:18:39 -0700

    all tests pass

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const questions = require("./lib/questions");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,7 +11,16 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { inherits } = require("util");
 
+let people = [];
+
+inquirer.prompt(questions).then((input) => {
+  let e = new Employee(input.name, input.id, input.email);
+  people.push(e);
+  const htmlTemplate = render(people);
+  fs.writeFile(outputPath, htmlTemplate, () => {});
+});
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
