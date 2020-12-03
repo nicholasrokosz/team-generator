b995453cdb8f1e52e070d8df4ea8d76599fc17d6
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-12-02 18:14:22 -0700

    polish app.js

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const makeEmployee = require("./lib/makeEmployee");
-const render = require("./lib/htmlRenderer");
+const makeFile = require("./lib/makeFile");
 const questions = require("./lib/questions");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const employees = [];
 
@@ -15,12 +10,7 @@ const init = async () => {
   const emp = makeEmployee(person);
   employees.push(emp);
   if (person.another) init();
-  else {
-    const htmlTemplate = render(employees);
-    fs.writeFile(outputPath, htmlTemplate, (err) => {
-      err ? console.log(err) : console.log("Success!");
-    });
-  }
+  else makeFile(employees);
 };
 
 init();
