a049aeec83d3232e939ee1ed4e86f924649976bf
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-12-02 22:21:29 -0700

    add requirment for manager

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const makeEmployee = require("./lib/makeEmployee");
 const makeFile = require("./lib/makeFile");
-const questions = require("./lib/questions");
+const { questions, managerQs } = require("./lib/questions");
 const inquirer = require("inquirer");
 
 const employees = [];
 
-const init = async () => {
+const addEmployee = async () => {
   const person = await inquirer.prompt(questions);
   employees.push(makeEmployee(person));
-  person.another ? init() : makeFile(employees);
+  person.another ? addEmployee() : makeFile(employees);
+};
+
+const init = async () => {
+  const person = await inquirer.prompt(managerQs);
+  employees.push(makeEmployee(person));
+  console.log("-----------------------------");
+  addEmployee();
 };
 
 init();
